300336039abeeb3f99fc3eff7b71b7733cf6e35d
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2020-11-13 20:26:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Test extends React.Component {
+  render() {
+    return <h1>Danylo Gera</h1>;
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'));
